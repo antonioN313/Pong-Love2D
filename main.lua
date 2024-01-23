7edf2e187ac912791c8bf9dd50c8fc91b3f4f202
antonioN313 <antoniocarlosfilho2003@gmail.com>
2024-01-23 13:39:19 -0300

    update: pong-3 paddle update

--- main.lua
+++ main.lua
@@ -10,6 +10,7 @@ WINDOW_HEIGHT = 720
 
 VIRTUAL_WIDTH = 432
 VIRTUAL_HEIGHT = 243
+PADDLE_SPEED = 200
 --[[
     Runs when the game first starts up, only once; used to initialize the game.
 ]]
@@ -27,9 +28,29 @@ function love.load()
         fullscreen = false,
         resizable = false,
         vsync = true
+
+	player1Score = 0
+	player2Score = 0
+
+	player1Y = 30
+	player2Y = VIRTUAL_HEIGHT - 50
     })
 end
 
+function love.update(dt)
+	-- player 1 movement
+	if love.keyboard.isDown('w') then
+		player1Y += -PADDLE_SPEED * dt
+	elseif love.keyboard.isDown('s') then
+		player1Y += PADDLE_SPEED *dt
+	end
+	-- player 2 movement
+	if love.keyboard.isDown('up') then
+		player2Y += -PADDLE_SPEED * dt
+	elseif love.keyboard.isDown('down') then
+		player2Y += PADDLE_SPEED *dt
+	end
+end
 function love.keypressed(key)
     if key == 'escape' then
         love.event.quit()
@@ -47,18 +68,20 @@ function love.draw()
     love.graphics.clear(0/255, 0/255, 0/255, 255/255)
 
     -- draw welcome text toward the top of the screen
+    love.graphics.setFont(smallFont)
     love.graphics.printf('Hello Pong!', 0, 20, VIRTUAL_WIDTH, 'center')
-
-    --
+    -- draw score left and right center of the screen
+    love.graphics.setFont(scoreFont)
+    love.graphics.print(tostring(player1Score),VIRTUAL_WIDTH / 2 - 50, VIRTUAL_HEIGHT /3)
+    love.graphics.print(tostring(player2Score), VIRTUAL_WIDTH / 2 + 30, VIRTUAL_HEIGHT / 3)
     -- paddles are simply rectangles we draw on the screen at certain points,
     -- as is the ball
-    --
 
     -- render first paddle (left side)
-    love.graphics.rectangle('fill', 10, 30, 5, 20)
+    love.graphics.rectangle('fill', 10,player1Y, 5, 20)
 
     -- render second paddle (right side)
-    love.graphics.rectangle('fill', VIRTUAL_WIDTH - 10, VIRTUAL_HEIGHT - 50, 5, 20)
+    love.graphics.rectangle('fill', VIRTUAL_WIDTH - 10,player2Y, 5, 20)
 
     -- render ball (center)
     love.graphics.rectangle('fill', VIRTUAL_WIDTH / 2 - 2, VIRTUAL_HEIGHT / 2 - 2, 4, 4)
