46eab5c5f7f2a155c1937298119bd71dbdc1ac62
antonioN313 <antoniocarlosfilho2003@gmail.com>
2024-01-23 16:05:52 -0300

    feature: pong-3 players movement

--- main.lua
+++ main.lua
@@ -10,18 +10,50 @@ WINDOW_HEIGHT = 720
 
 VIRTUAL_WIDTH = 432
 VIRTUAL_HEIGHT = 243
+
+PADDLE_SPEED = 200
 --[[
     Runs when the game first starts up, only once; used to initialize the game.
 ]]
 function love.load()
-    love.graphics.setDefaultFilter('nearest', 'nearest') 
-    push:setupScreen(VIRTUAL_WIDTH,VIRTUAL_HEIGHT,WINDOW_WIDTH, WINDOW_HEIGHT, {
+    love.graphics.setDefaultFilter('nearest', 'nearest')
+
+    -- more "retro-looking" font object we can use for any text
+    smallFont = love.graphics.newFont('font.ttf', 8)
+
+    scoreFont = love.graphics.newFont('font.ttf', 32)
+
+    -- set LÖVE2D's active font to the smallFont obect
+    love.graphics.setFont(smallFont)
+
+    -- initialize window with virtual resolution
+    push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
         fullscreen = false,
         resizable = false,
         vsync = true
     })
+
+    player1Score = 0
+    player2Score = 0
+
+    player1Y = 30
+    player2Y = VIRTUAL_HEIGHT - 50
 end
 
+function love.update(dt)
+	-- player 1 movement
+	if love.keyboard.isDown('w') then
+		player1Y = player1Y + -PADDLE_SPEED * dt
+	elseif love.keyboard.isDown('s') then
+		player1Y = player1Y + PADDLE_SPEED *dt
+	end
+	-- player 2 movement
+	if love.keyboard.isDown('up') then
+		player2Y = player2Y + -PADDLE_SPEED * dt
+	elseif love.keyboard.isDown('down') then
+		player2Y = player2Y + PADDLE_SPEED *dt
+	end
+end
 function love.keypressed(key)
     if key == 'escape' then
         love.event.quit()
@@ -31,12 +63,32 @@ end
     Called after update by LÖVE2D, used to draw anything to the screen, updated or otherwise.
 ]]
 function love.draw()
+    -- begin rendering at virtual resolution
     push:apply('start')
-    love.graphics.printf(
-        'Hello Pong!',          -- text to render
-        0,                      -- starting X (0 since we're going to center it based on width)
-        WINDOW_HEIGHT / 2 - 6,  -- starting Y (halfway down the screen)
-        WINDOW_WIDTH,           -- number of pixels to center within (the entire screen here)
-        'center')               -- alignment mode, can be 'center', 'left', or 'right'
+
+    -- clear the screen with a specific color; in this case, a color similar
+    -- to some versions of the original Pong
+    love.graphics.clear(0/255, 0/255, 0/255, 255/255)
+
+    -- draw welcome text toward the top of the screen
+    love.graphics.setFont(smallFont)
+    love.graphics.printf('Hello Pong!', 0, 20, VIRTUAL_WIDTH, 'center')
+    -- draw score left and right center of the screen
+    love.graphics.setFont(scoreFont)
+    love.graphics.print(tostring(player1Score),VIRTUAL_WIDTH / 2 - 50, VIRTUAL_HEIGHT /3)
+    love.graphics.print(tostring(player2Score), VIRTUAL_WIDTH / 2 + 30, VIRTUAL_HEIGHT / 3)
+    -- paddles are simply rectangles we draw on the screen at certain points,
+    -- as is the ball
+
+    -- render first paddle (left side)
+    love.graphics.rectangle('fill', 10,player1Y, 5, 20)
+
+    -- render second paddle (right side)
+    love.graphics.rectangle('fill', VIRTUAL_WIDTH - 10,player2Y, 5, 20)
+
+    -- render ball (center)
+    love.graphics.rectangle('fill', VIRTUAL_WIDTH / 2 - 2, VIRTUAL_HEIGHT / 2 - 2, 4, 4)
+
+    -- end rendering at virtual resolution
     push:apply('end')
-    end
+end
