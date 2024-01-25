26dbd8ebe37f7e9fd176842c8ae206442f154f94
antonioN313 <47403160+antonioN313@users.noreply.github.com>
2024-01-25 19:58:35 -0300

    fix: peddle classes

--- Paddle.lua
+++ Paddle.lua
@@ -8,7 +8,7 @@ function Paddle:init(x, y, width, height)
     self.dy = 0
 end
 
-function Paddle:update( ... )
+function Paddle:update(dt)
     if self.dy < 0 then
         self.y = math.max(0, self.y + self.dy * dt)
     else
